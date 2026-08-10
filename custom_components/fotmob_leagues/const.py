45d4ef8457bd70fc44713bfe298107984ffdc750
Martin Gøranson <30817386+isimagan@users.noreply.github.com>
2026-08-10 15:49:07 +0200

    Remove unused player statistics endpoint

--- custom_components/fotmob_leagues/const.py
+++ custom_components/fotmob_leagues/const.py
@@ -5,5 +5,4 @@ DOMAIN = "fotmob_leagues"
 CONF_LEAGUE_ID = "league_id"
 
 API_URL = "https://www.fotmob.com/api/data/leagues"
-DEEP_STATS_API_URL = "https://www.fotmob.com/api/data/leagueseasondeepstats"
 UPDATE_INTERVAL_MINUTES = 30
