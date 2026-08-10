f09903131a46d05a9b3866476ac78c642380dc89
Martin Gøranson <30817386+isimagan@users.noreply.github.com>
2026-08-10 16:04:10 +0200

    Add the league logo URL

--- custom_components/fotmob_leagues/const.py
+++ custom_components/fotmob_leagues/const.py
@@ -5,4 +5,7 @@ DOMAIN = "fotmob_leagues"
 CONF_LEAGUE_ID = "league_id"
 
 API_URL = "https://www.fotmob.com/api/data/leagues"
+LEAGUE_LOGO_URL = (
+    "https://images.fotmob.com/image_resources/logo/leaguelogo/{league_id}.png"
+)
 UPDATE_INTERVAL_MINUTES = 30
